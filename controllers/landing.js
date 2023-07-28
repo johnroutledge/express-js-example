b671ceb192266af2955acd1c760003eff1e7a19a
johnroutledge <78544640+johnroutledge@users.noreply.github.com>
2023-07-28 14:30:08 +0100

    Added lead edit and post routes

--- controllers/landing.js
+++ controllers/landing.js
@@ -23,7 +23,6 @@ exports.show_leads = function(req, res, next) {
 }
 
 exports.show_lead = function(req, res, next) {
-  console.log('Lead id:' + req.params.lead_id);
   return models.Lead.findOne({
     where : {
       id : req.params.lead_id
@@ -33,3 +32,31 @@ exports.show_lead = function(req, res, next) {
   });
 
 }
+
+exports.show_edit_lead = function(req, res, next) {
+  return models.Lead.findOne({
+    where : {
+      id : req.params.lead_id
+    }
+  }).then(lead => {
+    res.render('lead/edit_lead', { lead : lead});
+  });
+
+}
+
+exports.edit_lead = function(req, res, next) {
+
+  return models.Lead.update({
+    email: req.body.lead_email
+  }, {
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(result => {
+    res.redirect('/lead/' + req.params.lead_id);
+  })
+}
+
+
+
+
