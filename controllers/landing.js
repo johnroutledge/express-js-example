1bd7e11a83492654e8860578b7bd9aa751231c14
johnroutledge <78544640+johnroutledge@users.noreply.github.com>
2023-08-04 14:42:57 +0100

    Implemented login and logout routes

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,7 +2,7 @@
 const models = require('../models')
 
 exports.get_landing = function(req, res, next) {
-  res.render('landing', { title: 'Express' });
+  res.render('landing', { title: 'Express', user: req.user });
 }
 
 exports.submit_lead = function(req, res, next) {
@@ -12,14 +12,12 @@ exports.submit_lead = function(req, res, next) {
   }).then(lead => {
     res.redirect('/leads');
   })
-
 }
 
 exports.show_leads = function(req, res, next) {
-  models.Lead.findAll().then(leads => {
-      res.render('lead/leads', { title: 'Express', leads: leads });
+  return models.Lead.findAll().then(leads => {
+    res.render('lead/leads', { title: 'Express', leads: leads });   
   })
-
 }
 
 exports.show_lead = function(req, res, next) {
@@ -28,9 +26,8 @@ exports.show_lead = function(req, res, next) {
       id : req.params.lead_id
     }
   }).then(lead => {
-    res.render('lead/lead', { lead : lead});
+    res.render('lead/lead', { lead : lead });
   });
-
 }
 
 exports.show_edit_lead = function(req, res, next) {
@@ -39,16 +36,15 @@ exports.show_edit_lead = function(req, res, next) {
       id : req.params.lead_id
     }
   }).then(lead => {
-    res.render('lead/edit_lead', { lead : lead});
+    res.render('lead/edit_lead', { lead : lead });
   });
-
 }
 
 exports.edit_lead = function(req, res, next) {
 
   return models.Lead.update({
     email: req.body.lead_email
-  }, {
+  }, { 
     where: {
       id: req.params.lead_id
     }
@@ -56,9 +52,7 @@ exports.edit_lead = function(req, res, next) {
     res.redirect('/lead/' + req.params.lead_id);
   })
 }
-
 exports.delete_lead = function(req, res, next) {
-
   return models.Lead.destroy({
     where: {
       id: req.params.lead_id
@@ -69,17 +63,11 @@ exports.delete_lead = function(req, res, next) {
 }
 
 exports.delete_lead_json = function(req, res, next) {
-
   return models.Lead.destroy({
     where: {
       id: req.params.lead_id
     }
   }).then(result => {
-    res.send({ msg: "Success"});
+    res.send({ msg: "Success" });
   })
 }
-
-
-
-
-
